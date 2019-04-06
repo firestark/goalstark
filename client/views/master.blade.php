23e0f33b1cf1ddac19edc61d57570f1883520aa9
Aron Wouters <lionar@mail.com>
2019-04-06 23:53:58 +0200

    Changed font to exo 2

--- client/views/master.blade.php
+++ client/views/master.blade.php
@@ -16,6 +16,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="/resources/styles.css">
 
