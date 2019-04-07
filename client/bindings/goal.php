cc3b47bd69998d514976c64572d0237753f19fd0
Aron Wouters <lionar@mail.com>
2019-04-07 12:51:56 +0200

    Corrected syntax error

--- client/bindings/goal.php
+++ client/bindings/goal.php
@@ -5,6 +5,6 @@ app::bind ( goal::class, function ( )
     return new goal ( 
         input::get ( 'id', uniqid ( ) ),
         input::get ( 'description', '' ),
-        input::get ( 'completeBy', 0 ),
+        input::get ( 'completeBy', 0 )
     );
 } );
