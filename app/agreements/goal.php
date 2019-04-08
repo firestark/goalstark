4ac62dad7822f6679d75fb844845745d7240ade3
Aron Wouters <lionar@mail.com>
2019-04-08 11:52:52 +0200

    Setting goal completeBy timestamp to end of day.

--- app/agreements/goal.php
+++ app/agreements/goal.php
@@ -8,7 +8,17 @@ class goal
     {
         $this->id           = $id;
         $this->description  = $description;
-        $this->completeBy   = $completeBy;
+        $this->completeBy   = $this->completeByEndOfDay ( $completeBy );
         $this->completed    = $completed;
+    }
+
+    /**
+     * Setting the completeBy timestamp to the end of the day.
+     * This makes the goal only overdue based on the day.
+     */
+    private function completeByEndOfDay ( int $timestamp ) : int
+    {
+        $beginOfDay = strtotime ( 'midnight', $timestamp );
+        return strtotime ( 'tomorrow', $beginOfDay ) - 1;
     }
 }
