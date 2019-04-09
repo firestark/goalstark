e481ce0be8c156eb94f15967fab178e3ec7dfc61
Aron Wouters <lionar@mail.com>
2019-04-09 23:59:20 +0200

    Default sorting by completion time

--- client/statuses/1000. successfully found goals.php
+++ client/statuses/1000. successfully found goals.php
@@ -5,6 +5,14 @@ use function compact as with;
 
 status::matching ( 1000, function ( array $goals )
 {
+	usort ( $goals, function ( goal $a, goal $b )
+	{
+		if ( $a->completeBy === $b->completeBy )
+			return 0;
+
+		return ( $a->completeBy < $b->completeBy ) ? 1 : -1;
+	} );
+
 	if ( request::header ( 'Content-Type' ) === 'application/json' )
 		return json::ok ( array_values ( $goals ) );
 
