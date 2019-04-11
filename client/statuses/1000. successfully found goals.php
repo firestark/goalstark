b84fa4ce7e84cd3cb230e559592a14ab27930a5e
Aron Wouters <lionar@mail.com>
2019-04-11 18:08:14 +0200

    Sort on last added

--- client/statuses/1000. successfully found goals.php
+++ client/statuses/1000. successfully found goals.php
@@ -5,14 +5,8 @@ use function compact as with;
 
 status::matching ( 1000, function ( array $goals )
 {
-	usort ( $goals, function ( goal $a, goal $b )
-	{
-		if ( $a->completeBy === $b->completeBy )
-			return 0;
-
-		return ( $a->completeBy < $b->completeBy ) ? 1 : -1;
-	} );
-
+	$goals = array_reverse ( $goals );
+	
 	if ( request::header ( 'Content-Type' ) === 'application/json' )
 		return json::ok ( array_values ( $goals ) );
 
