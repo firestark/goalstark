0bdad3c53806556030791678ccdca94fb74327d2
Aron Wouters <lionar@mail.com>
2019-04-13 12:55:32 +0200

    Corrected consistency in code style

--- client/routes/POST -{id}.php
+++ client/routes/POST -{id}.php
@@ -2,6 +2,6 @@
 
 route::post ( '/{id}', function ( )
 {
-    input::set ( 'completeBy', strtotime ( input::get ( 'completeBy', date('Y-m-d') ) ) );
+    input::set ( 'completeBy', strtotime ( input::get ( 'completeBy', date ( 'Y-m-d' ) ) ) );
     return app::fulfill ( 'i want to update a goal' );
 } );
