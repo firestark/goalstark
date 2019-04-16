33c4cd5c07b5f0758559f57bbacaedfeaed2e822
Aron Wouters <lionar@mail.com>
2019-04-16 15:18:08 +0200

    Corrected array_reverse misplacement.

--- client/statuses/3000 - 4999 tasks/3002. successfully found a  task.php
+++ client/statuses/3000 - 4999 tasks/3002. successfully found a  task.php
@@ -4,6 +4,5 @@ use function compact as with;
 
 status::matching ( 3002, function ( task $task )
 {
-	$tasks = array_reverse ( $tasks );
 	return view::ok ( 'tasks.edit', with ( 'task' ) );
 } );
--- client/statuses/3000 - 4999 tasks/3006. successfully found tasks.php
+++ client/statuses/3000 - 4999 tasks/3006. successfully found tasks.php
@@ -4,5 +4,6 @@ use function compact as with;
 
 status::matching ( 3006, function ( array $tasks )
 {
+	$tasks = array_reverse ( $tasks );
 	return view::ok ( 'tasks.list', with ( 'tasks' ) );
 } );
