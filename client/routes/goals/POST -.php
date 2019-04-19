2fe64babd9be063a645ce4b49deff8720e24b656
Aron Wouters <lionar@mail.com>
2019-04-19 16:15:59 +0200

    Removed stale code

--- client/routes/goals/POST -.php
+++ client/routes/goals/POST -.php
@@ -2,6 +2,5 @@
 
 route::post ( '/', function ( )
 {
-    input::set ( 'completeBy', strtotime ( input::get ( 'completeBy', date ( 'Y-m-d' ) ) ) );
     return app::fulfill ( 'i want to add a new goal' );
 } );
--- client/routes/goals/POST -{id}.php
+++ client/routes/goals/POST -{id}.php
@@ -2,6 +2,5 @@
 
 route::post ( '/{id}', function ( )
 {
-    input::set ( 'completeBy', strtotime ( input::get ( 'completeBy', date ( 'Y-m-d' ) ) ) );
     return app::fulfill ( 'i want to update a goal' );
 } );
