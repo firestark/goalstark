6fe80113438652aed45d65f72e782caa6aa6c805
Aron Wouters <lionar@mail.com>
2019-04-24 16:44:24 +0200

    Renamed menu label

--- client/views/apps/food-tracker/page/overview.blade.php
+++ client/views/apps/food-tracker/page/overview.blade.php
@@ -29,7 +29,7 @@
                                 <path fill="none" d="M0 0h24v24H0V0z"/>
                                 <path d="M4 18h4c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1zm1 6h10c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1z"/>
                             </svg>
-                            <span>Registration</span>
+                            <span>Consumation</span>
                         </a>
                     </li>
                     <li>
