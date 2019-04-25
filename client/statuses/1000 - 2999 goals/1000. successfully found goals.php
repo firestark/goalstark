f003ad02185ad6aff99f14782060582a06998a34
Aron Wouters <lionar@mail.com>
2019-04-25 13:06:54 +0200

    Sort goals by rating

--- client/statuses/1000 - 2999 goals/1000. successfully found goals.php
+++ client/statuses/1000 - 2999 goals/1000. successfully found goals.php
@@ -4,6 +4,13 @@ use function compact as with;
 
 status::matching ( 1000, function ( array $goals )
 {
-	$goals = array_reverse ( $goals );
+	usort ( $goals, function ( $a, $b )
+	{
+		if ( $a->rating === $b->rating )
+			return 0;
+
+		return ( $a->rating > $b->rating ) ? -1 : 1;
+	} );
+
 	return view::ok ( 'goals.list', with ( 'goals' ) );
 } );
--- client/statuses/1000 - 2999 goals/1007. successfully found goals to select.php
+++ client/statuses/1000 - 2999 goals/1007. successfully found goals to select.php
@@ -4,6 +4,13 @@ use function compact as with;
 
 status::matching ( 1007, function ( array $goals )
 {
-	$goals = array_reverse ( $goals );
+	usort ( $goals, function ( $a, $b )
+	{
+		if ( $a->rating === $b->rating )
+			return 0;
+
+		return ( $a->rating > $b->rating ) ? -1 : 1;
+	} );
+	
 	return view::ok ( 'tasks.select-goal', with ( 'goals' ) );
 } );
