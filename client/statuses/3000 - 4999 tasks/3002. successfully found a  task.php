cc63a2719efeb1845b247a0b5ed144ec53052f81
Aron Wouters <lionar@mail.com>
2019-04-29 11:24:05 +0200

    Corrected returned view

--- client/statuses/3000 - 4999 tasks/3002. successfully found a  task.php
+++ client/statuses/3000 - 4999 tasks/3002. successfully found a  task.php
@@ -5,9 +5,5 @@ use function compact as with;
 status::matching ( 3002, function ( task $task )
 {
 	view::title ( 'Task details' );
-
-	if ( $task instanceof task\protein )
-		return view::ok ( 'tasks.protein.edit', with ( 'task' ) );
-	
 	return view::ok ( 'tasks.edit', with ( 'task' ) );
 } );
