4c471cf1b18e707dc501e57fcac245db271dbc78
Aron Wouters <lionar@mail.com>
2019-04-30 15:05:31 +0200

    Removed existence check

--- client/statuses/3000 - 4999 tasks/3006. successfully found tasks.php
+++ client/statuses/3000 - 4999 tasks/3006. successfully found tasks.php
@@ -5,14 +5,10 @@ use function compact as with;
 status::matching ( 3006, function ( array $tasks )
 {
 	$protein = 0;
-
-	if ( app::bound ( 'dietitian' ) )
-	{
-		$consumations = app::make ( 'dietitian' )->today ( );
-		
-		foreach ( $consumations as $consumation )
-			$protein += $consumation->product->nutrition->protein;
-	}
+	$consumations = app::make ( 'dietitian' )->today ( );
+	
+	foreach ( $consumations as $consumation )
+		$protein += $consumation->product->nutrition->protein;
 
 
 	$tasks = array_reverse ( $tasks );
