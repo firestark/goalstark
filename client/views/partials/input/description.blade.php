dccf435dea1d93676bb3374389884907ab21849f
Aron Wouters <lionar@mail.com>
2019-05-01 23:56:50 +0200

    Added helper text for description field

--- client/views/partials/input/description.blade.php
+++ client/views/partials/input/description.blade.php
@@ -1,11 +1,16 @@
-<div id="description-field" class="mdc-text-field mdc-text-field--textarea">
-    <textarea id="description" name="description" class="mdc-text-field__input" required>{{ $value }}</textarea>
-    <div class="mdc-notched-outline">
-        <div class="mdc-notched-outline__leading"></div>
-        <div class="mdc-notched-outline__notch">
-            <label for="goal" class="mdc-floating-label {{ ( $value ) ?  'mdc-floating-label--float-above' : '' }}">Description</label>
+<div>
+    <div id="description-field" class="mdc-text-field mdc-text-field--textarea">
+        <textarea id="description" name="description" class="mdc-text-field__input" required>{{ $value }}</textarea>
+        <div class="mdc-notched-outline">
+            <div class="mdc-notched-outline__leading"></div>
+            <div class="mdc-notched-outline__notch">
+                <label for="goal" class="mdc-floating-label {{ ( $value ) ?  'mdc-floating-label--float-above' : '' }}">Description</label>
+            </div>
+            <div class="mdc-notched-outline__trailing"></div>
         </div>
-        <div class="mdc-notched-outline__trailing"></div>
+    </div>
+    <div class="mdc-text-field-helper-line">
+        <div class="mdc-text-field-helper-text mdc-text-field-helper-text--persistent">Define your goal in specifics</div>
     </div>
 </div>
 
