dadb71832fbbb5ce5239764d03342ea4e2704929
Aron Wouters <lionar@mail.com>
2019-05-06 22:39:41 +0200

    Updated add goal page

--- client/views/goals/add.blade.php
+++ client/views/goals/add.blade.php
@@ -1,21 +1,21 @@
-@extends('page.details')
+@extends( 'page.overview' )
 
 @section ( 'navigation' )
-    @include ( 'partials.up-arrow', [ 'link' => '/' ] )
+    @include ( 'partials.up-arrow', [ 'link' => '/goals/' ] )
+@endsection
+
+@section ( 'title' )
+    Add goal
 @endsection
 
 @section ( 'content' )
-    <section class="mdc-card form-section" style="margin-bottom: 32px;">
-        <form method="POST" action="/">
-            <div id="title-and-due">
-                @include ( 'partials.input.title', [ 'value' => '' ] )
-                @include ( 'partials.input.due', [ 'value' => time ( ) ] )
-            </div>
-            
-            @include ( 'partials.input.description', [ 'value' => '' ] )
-            @include ( 'partials.input.reasons', [ 'value' => '' ] )
-            @include ( 'partials.input.rating', [ 'value' => 1, 'caption' => 'Importance' ] )
-            @include ( 'partials.input.fab', [ 'action' => 'save' ] )            
-        </form>
-    </section>
+    <form method="POST" action="/" style="padding: 8px;">
+        @include ( 'partials.input.title', [ 'value' => '' ] )
+        @include ( 'partials.input.due', [ 'value' => time ( ) ] )
+        
+        @include ( 'partials.input.description', [ 'value' => '' ] )
+        @include ( 'partials.input.reasons', [ 'value' => '' ] )
+        @include ( 'partials.input.rating', [ 'value' => 1, 'caption' => 'Importance' ] )
+        @include ( 'partials.input.fab', [ 'action' => 'save' ] )            
+    </form>
 @endsection
