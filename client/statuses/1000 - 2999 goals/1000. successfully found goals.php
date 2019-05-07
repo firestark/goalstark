446dc9bd6fc29742f8a2f54f4a9d220d0121126a
Aron Wouters <lionar@mail.com>
2019-05-07 12:12:43 +0200

    Removed rating remains

--- client/statuses/1000 - 2999 goals/1000. successfully found goals.php
+++ client/statuses/1000 - 2999 goals/1000. successfully found goals.php
@@ -4,13 +4,5 @@ use function compact as with;
 
 status::matching ( 1000, function ( array $goals )
 {
-	usort ( $goals, function ( $a, $b )
-	{
-		if ( $a->rating === $b->rating )
-			return 0;
-
-		return ( $a->rating > $b->rating ) ? -1 : 1;
-	} );
-
 	return view::ok ( 'goals.list', with ( 'goals' ) );
 } );
--- client/statuses/3000 - 4999 tasks/3008. successfully found goals to select.php
+++ client/statuses/3000 - 4999 tasks/3008. successfully found goals to select.php
@@ -4,13 +4,5 @@ use function compact as with;
 
 status::matching ( 3008, function ( array $goals )
 {	
-	usort ( $goals, function ( $a, $b )
-	{
-		if ( $a->rating === $b->rating )
-			return 0;
-
-		return ( $a->rating > $b->rating ) ? -1 : 1;
-	} );
-	
 	return view::ok ( 'tasks.wizard.select-goals', with ( 'goals' ) );
 } );
