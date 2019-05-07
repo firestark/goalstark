098afd21cbe05d11c11c2b69fbe687ff28ecf815
Aron Wouters <lionar@mail.com>
2019-05-07 10:52:28 +0200

    Improved message

--- client/statuses/1000 - 2999 goals/1007. successfully removed a task from a goal.php
+++ client/statuses/1000 - 2999 goals/1007. successfully removed a task from a goal.php
@@ -2,6 +2,6 @@
 
 status::matching ( 1007, function ( )
 {
-	session::flash ( 'message', 'Task removed.' );
+	session::flash ( 'message', 'Task removed from goal.' );
 	return redirect::back ( );
 } );
