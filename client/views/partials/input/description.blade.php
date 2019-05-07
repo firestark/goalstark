edb54368cf6009917ecd5a697bbc861c12d1f2b8
Aron Wouters <lionar@mail.com>
2019-05-07 14:47:50 +0200

    Made providing $value optional

--- client/views/partials/input/description.blade.php
+++ client/views/partials/input/description.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div id="description-field" class="mdc-text-field mdc-text-field--textarea">
-        <textarea id="description" name="description" class="mdc-text-field__input" required>{{ $value }}</textarea>
+        <textarea id="description" name="description" class="mdc-text-field__input" required>{{ $value ?? '' }}</textarea>
         <div class="mdc-notched-outline">
             <div class="mdc-notched-outline__leading"></div>
             <div class="mdc-notched-outline__notch">
-                <label for="goal" class="mdc-floating-label {{ ( $value ) ?  'mdc-floating-label--float-above' : '' }}">Description</label>
+                <label for="goal" class="mdc-floating-label {{ ( isset ( $value ) and $value ) ?  'mdc-floating-label--float-above' : '' }}">Description</label>
             </div>
             <div class="mdc-notched-outline__trailing"></div>
         </div>
