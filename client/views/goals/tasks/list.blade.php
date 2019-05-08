88852623f88d251eac1b1165f2b562d5bb43581e
Aron Wouters <lionar@mail.com>
2019-05-08 23:28:48 +0200

    Corrected empty tasks illustration

--- client/views/goals/tasks/list.blade.php
+++ client/views/goals/tasks/list.blade.php
@@ -93,7 +93,7 @@
             </svg>
 
             <h3 class="mdc-typography--subtitle2" style="font-weight: bold;">No tasks here</h3>
-            <p class="mdc-typography--body2" style="color: var(--mdc-theme-text-secondary-on-background); margin: 0; line-height: 10px;">Select some tasks by clicking the <span> 
+            <p class="mdc-typography--body2" style="color: var(--mdc-theme-text-secondary-on-background); margin: 0; line-height: 1.5rem;">Select some tasks by clicking the <span style="vertical-align: text-top;"> 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: var(--mdc-theme-text-secondary-on-background); position: relative; top: 4px;">
                     <path fill="none" d="M0 0h24v24H0V0z"/>
                     <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
