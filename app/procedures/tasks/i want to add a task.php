612cfbbd3e8c6346f76cf4dbfb978d58fc25939c
Aron Wouters <lionar@mail.com>
2019-05-09 09:19:15 +0200

    Allowing duplicates for due tasks

--- app/procedures/tasks/i want to add a task.php
+++ app/procedures/tasks/i want to add a task.php
@@ -4,7 +4,7 @@ when ( 'i want to add a task', then ( apply ( a (
     
 function ( task $task, task\manager $taskManager )
 {
-    if ( $taskManager->hasWithDescription ( $task->description ) )
+    if ( ! $task instanceof task\due and $taskManager->hasWithDescription ( $task->description ) )
         return [ 4001, [ ] ];
     
     $taskManager->add ( $task );
