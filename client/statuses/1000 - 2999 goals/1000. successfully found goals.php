709bb61eee5092acfd2a7e0c47c9724fe630c0bc
Aron Wouters <lionar@mail.com>
2019-05-11 00:53:50 +0200

    Added task count for goals

--- client/statuses/1000 - 2999 goals/1000. successfully found goals.php
+++ client/statuses/1000 - 2999 goals/1000. successfully found goals.php
@@ -4,6 +4,9 @@ use function compact as with;
 
 status::matching ( 1000, function ( array $goals )
 {
+	foreach ( $goals as $goal )
+		$goal->tasks = app::make ( task\manager::class )->tasksFor ( $goal );
+
 	$due = array_filter ( $goals, function ( $goal ) { return ! $goal->isOverdue ( ) and ! $goal->completed; } );
 	$overdue = array_filter ( $goals, function ( $goal ) { return $goal->isOverdue ( ); } );
 	$completed = array_filter ( $goals, function ( $goal ) { return $goal->completed; } );
--- client/views/goals/goal.blade.php
+++ client/views/goals/goal.blade.php
@@ -24,7 +24,7 @@
     <a href="/goals/{{ $goal->id }}">
         <span class="mdc-list-item__text" style="width: 100%;">
             <span class="mdc-list-item__primary-text">{{ $goal->title }}</span>
-            <span class="mdc-list-item__secondary-text"><span>Due {{  date ( 'M d, Y ', $goal->due ) }}</span>                                        
+            <span class="mdc-list-item__secondary-text"><span style="font-weight: bold;">Due {{  date ( 'M d, Y ', $goal->due ) }}</span> — {{ count ( $goal->tasks ) }} tasks</span>                                        
         </span>
     </a>
     
