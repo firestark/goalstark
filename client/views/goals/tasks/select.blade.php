25b75a99ae26fe4c94f85afe807e6cd3ca017b77
Aron Wouters <lionar@mail.com>
2019-05-11 14:56:20 +0200

    Corrected list styles

--- client/views/goals/tasks/select.blade.php
+++ client/views/goals/tasks/select.blade.php
@@ -11,47 +11,45 @@
 @section ( 'content' )
     
     @if ( count ( $tasks ) )
-        <form action="/goals/{{ $goalid }}/tasks" method="POST">
-            <section class="mdc-card">
-                <ul class="mdc-list" role="group">
-                    @foreach ( $tasks as $task )
-                        <li class="mdc-list-item" role="checkbox" aria-checked="{{ in_array ( $goalid, $task->goals ) ? 'true' : 'false' }}">
-                            <span class="mdc-list-item__graphic">
-                                <div class="mdc-checkbox">
-                                    <input 
-                                        type="checkbox"
-                                        class="mdc-checkbox__native-control"
-                                        id="{{ $task->id }}"
-                                        name="tasks[]"
-                                        value="{{ $task->id }}"
-                                        {{ in_array ( $goalid, $task->goals ) ? 'checked' : '' }}>
-                            
-                                    <div class="mdc-checkbox__background">
-                                        <svg class="mdc-checkbox__checkmark"
-                                            viewBox="0 0 24 24">
-                                            <path class="mdc-checkbox__checkmark-path"
-                                                fill="none"
-                                                d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
-                                        </svg>
-                                        <div class="mdc-checkbox__mixedmark"></div>
-                                    </div>
+        <form action="/goals/{{ $goalid }}/tasks" method="POST" style="padding: 16px 0 80px;">
+            <ul class="mdc-list" role="group">
+                @foreach ( $tasks as $task )
+                    <li class="mdc-list-item" role="checkbox" aria-checked="{{ in_array ( $goalid, $task->goals ) ? 'true' : 'false' }}">
+                        <span class="mdc-list-item__graphic">
+                            <div class="mdc-checkbox">
+                                <input 
+                                    type="checkbox"
+                                    class="mdc-checkbox__native-control"
+                                    id="{{ $task->id }}"
+                                    name="tasks[]"
+                                    value="{{ $task->id }}"
+                                    {{ in_array ( $goalid, $task->goals ) ? 'checked' : '' }}>
+                        
+                                <div class="mdc-checkbox__background">
+                                    <svg class="mdc-checkbox__checkmark"
+                                        viewBox="0 0 24 24">
+                                        <path class="mdc-checkbox__checkmark-path"
+                                            fill="none"
+                                            d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+                                    </svg>
+                                    <div class="mdc-checkbox__mixedmark"></div>
                                 </div>
-                            </span>
+                            </div>
+                        </span>
 
-                            <label class="mdc-list-item__text" for="{{ $task->id }}" style="width: 100%;">
-                                {{ $task->description }}
-                            </label>
-                        </li>
-                    
-                    @endforeach
-                </ul>
+                        <label class="mdc-list-item__text" for="{{ $task->id }}" style="width: 100%;">
+                            {{ $task->description }}
+                        </label>
+                    </li>
+                
+                @endforeach
+            </ul>
 
-                <div class="mdc-card__actions">
-                    <button type="submit" class="mdc-button mdc-card__action mdc-card__action--button">
-                        <span class="mdc-button__label">Choose</span>
-                    </button>
-                </div>
-            </section>
+            <div class="mdc-card__actions">
+                <button type="submit" class="mdc-button mdc-card__action mdc-card__action--button">
+                    <span class="mdc-button__label">Choose</span>
+                </button>
+            </div>
         </form>
     @endif
 
