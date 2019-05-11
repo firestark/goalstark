4b7859674b0732926bf2e97609efb4ff4f6243df
Aron Wouters <lionar@mail.com>
2019-05-11 03:57:15 +0200

    Corrected replace uri

--- client/app/dispatcher.php
+++ client/app/dispatcher.php
@@ -7,7 +7,7 @@ class dispatcher extends \http\dispatcher
     public function match ( string $method, string $uri )
     {
         if ( defined ( 'BASEURL' ) )
-            $uri = str_replace ( BASEURL, '/', $uri );
+            $uri = str_replace ( BASEURL, '', $uri );
 
         return parent::match ( $method, $uri );
     }
