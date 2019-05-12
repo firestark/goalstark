c7197845fd7d074d6b254d39e8be8e1776f02841
Aron Wouters <lionar@mail.com>
2019-05-12 02:22:53 +0200

    Removed unused uri

--- client/statuses/3000 - 4999 tasks/3006, 7009. successfully found tasks.php
+++ client/statuses/3000 - 4999 tasks/3006, 7009. successfully found tasks.php
@@ -8,10 +8,6 @@ status::matching ( [ 3006, 7009 ], function ( array $tasks, int $protein )
 	$today = array_filter ( $tasks, function ( $task ) { return $task->dueToday ( ); } );
 	$later = array_filter ( $tasks, function ( $task ) { return ! $task->isOverdue ( ) and ! $task->dueToday ( ); } );
 	$overdue = array_filter ( $tasks, function ( $task ) { return $task->isOverdue ( ); } );
-
-
-	if ( strpos ( request::uri ( ), '/tasks/add' ) !== false )
-		return view::ok ( 'goals.tasks.add', with ( 'tasks', 'today', 'later', 'protein' ) );
-
+	
 	return view::ok ( 'tasks.list', with ( 'tasks', 'today', 'later', 'overdue', 'protein' ) );
 } );
