d213b47c4de10e67c7380fb16787444f31959e64
Aron Wouters <lionar@mail.com>
2019-05-14 00:28:36 +0200

    Removed completed from 'later' tasks

--- client/statuses/3000 - 4999 tasks/3006, 7009. successfully found tasks.php
+++ client/statuses/3000 - 4999 tasks/3006, 7009. successfully found tasks.php
@@ -6,7 +6,7 @@ status::matching ( [ 3006, 7009 ], function ( array $tasks, int $protein )
 {
 	$tasks = array_reverse ( $tasks );
 	$today = array_filter ( $tasks, function ( $task ) { return $task->dueToday ( ); } );
-	$later = array_filter ( $tasks, function ( $task ) { return ! $task->isOverdue ( ) and ! $task->dueToday ( ); } );
+	$later = array_filter ( $tasks, function ( $task ) { return ! $task->isOverdue ( ) and ! $task->dueToday ( ) and ! $task->isCompleted ( ); } );
 	$overdue = array_filter ( $tasks, function ( $task ) { return $task->isOverdue ( ); } );
 	
 	return view::ok ( 'tasks.list', with ( 'tasks', 'today', 'later', 'overdue', 'protein' ) );
