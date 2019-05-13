3e788e64ea00499c75ffba4200c958fa488aedbb
Aron Wouters <lionar@mail.com>
2019-05-13 11:40:45 +0200

    Corrected list styling

--- client/views/apps/food-tracker/select-product.blade.php
+++ client/views/apps/food-tracker/select-product.blade.php
@@ -10,19 +10,17 @@
 
 @section ( 'content' )
     @if ( count ( $products ) )
-        <section class="mdc-card">
-            <ul class="mdc-list mdc-list--two-line mdc-list--linked">
-                @foreach ( $products as $product )
-                    <li class="mdc-list-item" tabindex="0">
-                        <a href="/apps/food-tracker/add/{{ $product->id }}">
-                            <span class="mdc-list-item__text">
-                                <span class="mdc-list-item__primary-text">{{ $product->name }}</span>
-                                <span class="mdc-list-item__secondary-text">{{ $product->nutrition->per }}</span>
-                            </span>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </section>
+        <ul class="mdc-list mdc-list--two-line mdc-list--linked">
+            @foreach ( $products as $product )
+                <li class="mdc-list-item" tabindex="0">
+                    <a href="/apps/food-tracker/add/{{ $product->id }}">
+                        <span class="mdc-list-item__text">
+                            <span class="mdc-list-item__primary-text">{{ $product->name }}</span>
+                            <span class="mdc-list-item__secondary-text">{{ $product->nutrition->per }}</span>
+                        </span>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
     @endif    
 @endsection
