86e0d2e1a23cc4af26d84c3412c354a01eab2738
Aron Wouters <lionar@mail.com>
2019-05-13 10:50:29 +0200

    Corrected html

--- client/views/tasks/wizard/select-type.blade.php
+++ client/views/tasks/wizard/select-type.blade.php
@@ -26,77 +26,75 @@
             <input type="hidden" name="goals[]" value="{{ $goal }}">
         @endforeach
 
-        <section class="mdc-card">
-            <ul class="mdc-list mdc-list--two-line" role="radiogroup">
-                <li class="mdc-list-item" role="radio" aria-checked="true" tabindex="1">
-                    <span class="mdc-list-item__graphic">
-                        <div class="mdc-radio">
-                            <input class="mdc-radio__native-control"
-                                type="radio"
-                                id="type-1"
-                                name="type"
-                                value="due"
-                                checked>
-                            <div class="mdc-radio__background">
-                                <div class="mdc-radio__outer-circle"></div>
-                                <div class="mdc-radio__inner-circle"></div>
-                            </div>
+        <ul class="mdc-list mdc-list--two-line" role="radiogroup">
+            <li class="mdc-list-item" role="radio" aria-checked="true" tabindex="1">
+                <span class="mdc-list-item__graphic">
+                    <div class="mdc-radio">
+                        <input class="mdc-radio__native-control"
+                            type="radio"
+                            id="type-1"
+                            name="type"
+                            value="due"
+                            checked>
+                        <div class="mdc-radio__background">
+                            <div class="mdc-radio__outer-circle"></div>
+                            <div class="mdc-radio__inner-circle"></div>
                         </div>
+                    </div>
+                </span>
+                <label class="mdc-list-item__text" for="type-1" style="width: 100%;">
+                    <span class="mdc-list-item__text" style="width: 100%;">
+                        <span class="mdc-list-item__primary-text">Due task</span>
+                        <span class="mdc-list-item__secondary-text">A due task executed once</span>
                     </span>
-                    <label class="mdc-list-item__text" for="type-1" style="width: 100%;">
-                        <span class="mdc-list-item__text" style="width: 100%;">
-                            <span class="mdc-list-item__primary-text">Due task</span>
-                            <span class="mdc-list-item__secondary-text">A due task executed once</span>
-                        </span>
-                    </label>
-                </li>
+                </label>
+            </li>
 
-                <li class="mdc-list-item" role="radio" aria-checked="true" tabindex="2">
-                    <span class="mdc-list-item__graphic">
-                        <div class="mdc-radio">
-                            <input class="mdc-radio__native-control"
-                                type="radio"
-                                id="type-2"
-                                name="type"
-                                value="daily"
-                                checked>
-                            <div class="mdc-radio__background">
-                                <div class="mdc-radio__outer-circle"></div>
-                                <div class="mdc-radio__inner-circle"></div>
-                            </div>
+            <li class="mdc-list-item" role="radio" aria-checked="true" tabindex="2">
+                <span class="mdc-list-item__graphic">
+                    <div class="mdc-radio">
+                        <input class="mdc-radio__native-control"
+                            type="radio"
+                            id="type-2"
+                            name="type"
+                            value="daily"
+                            checked>
+                        <div class="mdc-radio__background">
+                            <div class="mdc-radio__outer-circle"></div>
+                            <div class="mdc-radio__inner-circle"></div>
                         </div>
+                    </div>
+                </span>
+                <label class="mdc-list-item__text" for="type-2" style="width: 100%;">
+                    <span class="mdc-list-item__text" style="width: 100%;">
+                        <span class="mdc-list-item__primary-text">Daily task</span>
+                        <span class="mdc-list-item__secondary-text">A task that has to be done daily</span>
                     </span>
-                    <label class="mdc-list-item__text" for="type-2" style="width: 100%;">
-                        <span class="mdc-list-item__text" style="width: 100%;">
-                            <span class="mdc-list-item__primary-text">Daily task</span>
-                            <span class="mdc-list-item__secondary-text">A task that has to be done daily</span>
-                        </span>
-                    </label>
-                </li>
-    
-                <li class="mdc-list-item" role="radio" aria-checked="false" tabindex="3">
-                    <span class="mdc-list-item__graphic">
-                        <div class="mdc-radio">
-                            <input class="mdc-radio__native-control"
-                                type="radio"
-                                id="type-3"
-                                name="type"
-                                value="protein">
-                            <div class="mdc-radio__background">
-                                <div class="mdc-radio__outer-circle"></div>
-                                <div class="mdc-radio__inner-circle"></div>
-                            </div>
+                </label>
+            </li>
+
+            <li class="mdc-list-item" role="radio" aria-checked="false" tabindex="3">
+                <span class="mdc-list-item__graphic">
+                    <div class="mdc-radio">
+                        <input class="mdc-radio__native-control"
+                            type="radio"
+                            id="type-3"
+                            name="type"
+                            value="protein">
+                        <div class="mdc-radio__background">
+                            <div class="mdc-radio__outer-circle"></div>
+                            <div class="mdc-radio__inner-circle"></div>
                         </div>
+                    </div>
+                </span>
+                <label class="mdc-list-item__text" for="type-3" style="width: 100%;">
+                    <span class="mdc-list-item__text" style="width: 100%;">
+                        <span class="mdc-list-item__primary-text">Protein task</span>
+                        <span class="mdc-list-item__secondary-text">Daily protein consumption task</span>
                     </span>
-                    <label class="mdc-list-item__text" for="type-3" style="width: 100%;">
-                        <span class="mdc-list-item__text" style="width: 100%;">
-                            <span class="mdc-list-item__primary-text">Protein task</span>
-                            <span class="mdc-list-item__secondary-text">Daily protein consumption task</span>
-                        </span>
-                    </label>
-                </li>
-            </ul>
-        </section>
+                </label>
+            </li>
+        </ul>
 
         <section 
             style="
