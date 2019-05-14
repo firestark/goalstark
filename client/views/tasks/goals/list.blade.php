b2ba9b313fc456db83db3fdb846a4dbf9ac61779
Aron Wouters <lionar@mail.com>
2019-05-14 12:48:54 +0200

    Removed card styles

--- client/views/tasks/goals/list.blade.php
+++ client/views/tasks/goals/list.blade.php
@@ -41,50 +41,48 @@
 
 @section ( 'content' )
     @if ( count ( $goals ) )
-        <section class="mdc-card">
-            <ul class="mdc-list mdc-list--two-line mdc-list--avatar-list mdc-list--highlight mdc-list--linked">
-                @foreach ( $goals as $goal )
-                    <li class="mdc-list-item" tabindex="0">
+        <ul class="mdc-list mdc-list--two-line mdc-list--avatar-list mdc-list--highlight mdc-list--linked">
+            @foreach ( $goals as $goal )
+                <li class="mdc-list-item" tabindex="0">
 
-                        @if ( $goal->completed )
-                            <a  href="/goals/{{ $goal->id }}/uncomplete" 
-                                class="mdc-list-item__graphic completed" 
-                                aria-hidden="true">
-                                
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="none" d="M0 0h24v24H0V0z"/>
-                                    <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
-                                </svg>
-                            </a>
-                        @else
-                            <a  href="/goals/{{ $goal->id }}/complete" 
-                                class="mdc-list-item__graphic {{ ( $goal->due < time ( ) ) ? 'overdue' : '' }}" 
-                                aria-hidden="true">
-                                
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="none" d="M0 0h24v24H0V0z"/>
-                                    <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
-                                </svg>
-                            </a>
-                        @endif
-
-                        <a href="/goals/{{ $goal->id }}">
-                            <span class="mdc-list-item__text" style="width: 100%;">
-                                <span class="mdc-list-item__primary-text">{{ $goal->title }}</span>
-                                <span class="mdc-list-item__secondary-text"><span>Due {{  date ( 'M d, Y ', $goal->due ) }}</span>        
-                            </span>
-                        </a>
-                        
-                        <a href="/tasks/remove/{{ $taskid }}/{{ $goal->id }}" class="mdc-list-item__meta" aria-hidden="true">
+                    @if ( $goal->completed )
+                        <a  href="/goals/{{ $goal->id }}/uncomplete" 
+                            class="mdc-list-item__graphic completed" 
+                            aria-hidden="true">
+                            
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                 <path fill="none" d="M0 0h24v24H0V0z"/>
-                                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/>
+                                <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
                             </svg>
                         </a>
-                    </li>
-                @endforeach
-            </ul>
-        </section>
+                    @else
+                        <a  href="/goals/{{ $goal->id }}/complete" 
+                            class="mdc-list-item__graphic {{ ( $goal->due < time ( ) ) ? 'overdue' : '' }}" 
+                            aria-hidden="true">
+                            
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="none" d="M0 0h24v24H0V0z"/>
+                                <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
+                            </svg>
+                        </a>
+                    @endif
+
+                    <a href="/goals/{{ $goal->id }}">
+                        <span class="mdc-list-item__text" style="width: 100%;">
+                            <span class="mdc-list-item__primary-text">{{ $goal->title }}</span>
+                            <span class="mdc-list-item__secondary-text"><span>Due {{  date ( 'M d, Y ', $goal->due ) }}</span>        
+                        </span>
+                    </a>
+                    
+                    <a href="/tasks/remove/{{ $taskid }}/{{ $goal->id }}" class="mdc-list-item__meta" aria-hidden="true">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="none" d="M0 0h24v24H0V0z"/>
+                            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/>
+                        </svg>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
     @else
         <div style="text-align: center; margin: 56px auto 0; width: 230px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24" fill="var(--mdc-theme-secondary)" style="opacity: 0.7;">
