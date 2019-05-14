9bb8d022355ebaa9224b5c0407404d7be5302ee5
Aron Wouters <lionar@mail.com>
2019-05-14 13:44:36 +0200

    Improved button label

--- client/views/goals/tasks/add.blade.php
+++ client/views/goals/tasks/add.blade.php
@@ -67,7 +67,7 @@
             </div>
             <div style="display: flex; justify-content: flex-end;">
                 <button class="mdc-button" type="submit">
-                    <span class="mdc-button__label">DESCRIBE TASK</span>
+                    <span class="mdc-button__label">ADD TASK</span>
                     <svg class="mdc-button__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
                         <path d="M0 0h24v24H0z" fill="none"/>
