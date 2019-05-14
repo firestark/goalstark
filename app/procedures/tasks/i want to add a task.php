691a40ba73f6cf32e08fbbadeef16f60b1000245
Aron Wouters <lionar@mail.com>
2019-05-14 09:32:18 +0200

    Removed existence check

--- app/procedures/tasks/i want to add a task.php
+++ app/procedures/tasks/i want to add a task.php
@@ -3,10 +3,7 @@
 when ( 'i want to add a task', then ( apply ( a ( 
     
 function ( task $task, task\manager $taskManager )
-{
-    if ( ! $task instanceof task\due and $taskManager->hasWithDescription ( $task->description ) )
-        return [ 4001, [ ] ];
-    
+{   
     $taskManager->add ( $task );
     return [ 3000, [ 'id' => $task->id ] ];
 } ) ) ) );
