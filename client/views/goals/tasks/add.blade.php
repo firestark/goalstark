eb4efd838d45dc6ac10befc8335c8a45843a4f30
Aron Wouters <lionar@mail.com>
2019-05-14 13:56:39 +0200

    Corrected uri

--- client/views/goals/tasks/add.blade.php
+++ client/views/goals/tasks/add.blade.php
@@ -57,7 +57,7 @@
                 align-items: center;
                 background-color: var(--mdc-theme-background);">
             <div>
-                <a href="/goals/{$goalid}/tasks/select-type?{{ http_build_query ( input::all ( ) ) }}" class="mdc-button">
+                <a href="/goals/{{ $goalid }}/tasks/select-type?{{ http_build_query ( input::all ( ) ) }}" class="mdc-button">
                     <svg class="mdc-button__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
                         <path d="M0 0h24v24H0z" fill="none"/>
