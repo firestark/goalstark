d3c228f52727ccbee0848516d564d274300ab6c0
Aron Wouters <lionar@mail.com>
2019-05-14 11:25:32 +0200

    Refactored product count task

--- app/agreements/task/product/count.php
+++ app/agreements/task/product/count.php
@@ -4,58 +4,15 @@ namespace task\product;
 
 use product;
 
-class count extends \task
+class count extends \task\count
 {
-    /**
-     * An array of timestamps to track completion dates.
-     */
-    public $completions = [ ];
-    public $times = 0;
-    public $due = 0;
     public $product = null;
 
     function __construct ( array $goals, $id, product $product, int $times, int $due )
     {
         $this->product = $product;
-        $this->times = $times;
-        $this->due = endOfDay ( $due );
-
         $description = "Eat {$product->name}";
 
-        parent::__construct ( $goals, $id, $description );
-    }
-
-    function complete ( )
-    {
-        $this->completions [ ] = time ( );
-    }
-
-    function uncomplete ( )
-    {
-        array_pop ( $this->completions );
-    }
-
-    function isCompleted ( ) : bool
-    {
-        return ( count ( $this->completions ) >= $this->times );
-    }
-
-    function isOverdue ( ) : bool
-    {
-        return ( 
-            ! $this->isCompleted ( ) and 
-            $this->due < endOfDay ( time ( ) )
-        );
-    }
-
-    function dueToday ( ) : bool
-    {
-        return ( $this->due === endOfDay ( time ( ) ) );
-    }
-
-    private function endOfDay ( int $timestamp ) : int
-    {
-        $beginOfDay = strtotime ( 'midnight', $timestamp );
-        return strtotime ( 'tomorrow', $beginOfDay ) - 1;
+        parent::__construct ( $goals, $id, $description, $times, $due );
     }
 }
