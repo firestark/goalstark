765275ff8fccd7d714f2235aff1c5033e59fdf1a
Aron Wouters <lionar@mail.com>
2019-05-14 09:08:19 +0200

    Improved illustration text

--- client/views/goals/tasks/list.blade.php
+++ client/views/goals/tasks/list.blade.php
@@ -101,7 +101,7 @@
                     <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
                 </svg> 
                 </span> button
-                or create a new one with the
+                or create a new task with the
                 <span style="vertical-align: text-top;"> 
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: var(--mdc-theme-text-secondary-on-background); position: relative; top: 4px;">
                         <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
