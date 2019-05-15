2c16a187705cffb83c340864b066fcd780d62d91
Aron Wouters <lionar@mail.com>
2019-05-15 15:29:40 +0200

    Added description field to count task

--- client/views/tasks/types/count.blade.php
+++ client/views/tasks/types/count.blade.php
@@ -1,11 +1,18 @@
-<form method="POST" action="/tasks/{{ $task->id }}" style="height: 100%;">
+<form method="POST" action="/tasks/{{ $task->id }}">
     <section style="overflow: overlay; padding: 16px 8px 80px;">
         @foreach ( $task->goals as $goal )
             <input type="hidden" name="goals[]" value="{{ $goal }}">
         @endforeach
 
         @include ( 'partials.input.count', [ 'value' => $task->times ] )
+        @include ( 'partials.input.due', [ 'value' => $task->due ] )
+        @include ( 'partials.input.description', [ 'value' => $task->description ] )
         
         @include ( 'partials.input.fab', [ 'action' => 'save' ] )
     </section>
 </form>
+
+<section style="padding: 0 8px;">
+    <h2 class="mdc-typography--headline6">Completion status</h2>
+    <p>{{ count ( $task->completions ) }} of {{ $task->times }}</p>
+</section>
