10f037ef5896dd14f24c604e0049add11b2aaf94
Aron Wouters <lionar@mail.com>
2019-05-15 23:56:24 +0200

    Added chips

--- client/views/goals/tasks/task.blade.php
+++ client/views/goals/tasks/task.blade.php
@@ -51,6 +51,42 @@
                 <span class="mdc-list-item__secondary-text">Daily task</span>
             @endif
         </span>
+
+        <span style="
+            background-color: var(--mdc-theme-surface);
+            border-radius: 12px;
+            position: absolute;
+            bottom: 8px;
+            font-size: 12px;
+            color: var(--mdc-theme-on-surface);
+            right: 52px;
+            padding: 0 8px;">
+            
+            @switch ( $task )
+                @case ( $task instanceof task\product\count )
+                    Product count
+                    @break
+                
+                @case ( $task instanceof task\count )
+                    Count
+                    @break
+
+                @case ( $task instanceof task\protein )
+                    Protein
+                    @break
+
+                @case ( $task instanceof task\daily )
+                    Daily
+                    @break
+
+                @case ( $task instanceof task\due )
+                    Due
+                    @break
+
+                @default
+                    
+            @endswitch
+        </span>
     </a>
 
     <a href="/goals/{{ $goal->id }}/tasks/{{ $task->id }}/remove" class="mdc-list-item__meta" aria-hidden="true">
