671280591b85891bf65e7bf6e8c8676592771bc2
Aron Wouters <lionar@mail.com>
2019-05-15 23:21:26 +0200

    Refactored tab bar

---
+++ client/views/goals/tab-bar.blade.php
@@ -0,0 +1,40 @@
+@section ( 'top-app-bar' )
+    <div class="mdc-tab-bar mdc-tab-bar--top" role="tablist">
+        <div class="mdc-tab-scroller">
+            <div class="mdc-tab-scroller__scroll-area">
+                <div class="mdc-tab-scroller__scroll-content">
+                    
+                    <a href="/goals/{{ $goalid }}" class="mdc-tab {{ ( request::uri ( ) === "/goals/{$goalid}" ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
+                        <span class="mdc-tab__content">
+                            <span class="mdc-tab__text-label">Details</span>
+                        </span>
+                        <span class="mdc-tab-indicator {{ ( request::uri ( ) === "/goals/{$goalid}" ) ? 'mdc-tab-indicator--active' : '' }}">
+                            <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                        </span>
+                        <span class="mdc-tab__ripple"></span>
+                    </a>
+
+                    <a href="/goals/{{ $goalid }}/tasks" class="mdc-tab {{ ( request::uri ( ) === "/goals/{$goalid}/tasks" ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
+                        <span class="mdc-tab__content">
+                            <span class="mdc-tab__text-label">Tasks</span>
+                        </span>
+                        <span class="mdc-tab-indicator {{ ( request::uri ( ) === "/goals/{$goalid}/tasks" ) ? 'mdc-tab-indicator--active' : '' }}">
+                            <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                        </span>
+                        <span class="mdc-tab__ripple"></span>
+                    </a>
+
+                    <a href="/goals/{{ $goalid }}/reasons" class="mdc-tab {{ ( request::uri ( ) === "/goals/{$goalid}/reasons" ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
+                        <span class="mdc-tab__content">
+                            <span class="mdc-tab__text-label">Reasons</span>
+                        </span>
+                        <span class="mdc-tab-indicator {{ ( request::uri ( ) === "/goals/{$goalid}/reasons" ) ? 'mdc-tab-indicator--active' : '' }}">
+                            <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                        </span>
+                        <span class="mdc-tab__ripple"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- client/views/goals/tasks/list.blade.php
+++ client/views/goals/tasks/list.blade.php
@@ -16,46 +16,7 @@
     @include ( 'partials.up-arrow', [ 'link' => '/' ] )
 @endsection
 
-@section ( 'top-app-bar' )
-    <div class="mdc-tab-bar mdc-tab-bar--top" role="tablist">
-        <div class="mdc-tab-scroller">
-            <div class="mdc-tab-scroller__scroll-area">
-                <div class="mdc-tab-scroller__scroll-content">
-                    
-                    <a href="/goals/{{ $goal->id }}" class="mdc-tab {{ ( request::uri ( ) === "/goals/{$goal->id}" ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
-                        <span class="mdc-tab__content">
-                            <span class="mdc-tab__text-label">Details</span>
-                        </span>
-                        <span class="mdc-tab-indicator {{ ( request::uri ( ) === "/goals/{$goal->id}" ) ? 'mdc-tab-indicator--active' : '' }}">
-                            <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
-                        </span>
-                        <span class="mdc-tab__ripple"></span>
-                    </a>
-
-                    <a href="/goals/{{ $goal->id }}/tasks" class="mdc-tab {{ ( request::uri ( ) === "/goals/{$goal->id}/tasks" ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
-                        <span class="mdc-tab__content">
-                            <span class="mdc-tab__text-label">Tasks</span>
-                        </span>
-                        <span class="mdc-tab-indicator {{ ( request::uri ( ) === "/goals/{$goal->id}/tasks" ) ? 'mdc-tab-indicator--active' : '' }}">
-                            <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
-                        </span>
-                        <span class="mdc-tab__ripple"></span>
-                    </a>
-
-                    <a href="/goals/{{ $goal->id }}/reasons" class="mdc-tab {{ ( request::uri ( ) === "/goals/{$goal->id}/reasons" ) ? 'mdc-tab--active' : '' }}" role="tab" aria-selected="true" tabindex="0">
-                        <span class="mdc-tab__content">
-                            <span class="mdc-tab__text-label">Reasons</span>
-                        </span>
-                        <span class="mdc-tab-indicator {{ ( request::uri ( ) === "/goals/{$goal->id}/reasons" ) ? 'mdc-tab-indicator--active' : '' }}">
-                            <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
-                        </span>
-                        <span class="mdc-tab__ripple"></span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+@include ( 'goals.tab-bar', [ 'goalid' => $goal->id ] )
 
 @section ( 'content' )
     @if ( count ( $tasks ) )
