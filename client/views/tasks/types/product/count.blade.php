ca6b88f28afb1f8bbb547cad9784eef1c559b1e6
Aron Wouters <lionar@mail.com>
2019-05-15 21:53:19 +0200

    Improved counter

--- client/views/tasks/types/product/count.blade.php
+++ client/views/tasks/types/product/count.blade.php
@@ -14,14 +14,17 @@
     @include ( 'partials.input.fab', [ 'action' => 'save' ] )
 </form>
 
-<div style="font-size: 64px; text-align: center;">
-    <a href="/tasks/{{ $task->id }}/uncomplete">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="var(--mdc-theme-text-primary-on-background)">
-            <path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"/>
-            <path d="M4 11v2h8v-2H4zm15 7h-2V7.38L14 8.4V6.7L18.7 5h.3v13z"/>
-        </svg>
-    </a>
-    {{ count ( $task->completions ) }}
+<div style="text-align: center;">
+    <h2 class="mdc-typography--headline6">Consumations</h2>
+    @if ( ! count ( $task->completions ) < 1 )
+        <a href="/tasks/{{ $task->id }}/uncomplete">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="var(--mdc-theme-text-primary-on-background)">
+                <path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"/>
+                <path d="M4 11v2h8v-2H4zm15 7h-2V7.38L14 8.4V6.7L18.7 5h.3v13z"/>
+            </svg>
+        </a>
+    @endif
+    <span style="font-size: 64px;">{{ count ( $task->completions ) }}</span>
     <a href="/tasks/{{ $task->id }}/complete">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="var(--mdc-theme-text-primary-on-background)">
             <path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"/>
