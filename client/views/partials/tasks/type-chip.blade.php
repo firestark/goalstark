f72031445cd4f5ce48988b8cf0be066df7c15630
Aron Wouters <lionar@mail.com>
2019-05-16 10:40:13 +0200

    Removed chip color

--- client/views/partials/tasks/type-chip.blade.php
+++ client/views/partials/tasks/type-chip.blade.php
@@ -1,22 +1,22 @@
 @switch ( $task )
     @case ( $task instanceof task\product\count )
-        <span class="mdc-list-chip" style="background-color: #9c27b0; color: white;">Product count</span>
+        <span class="mdc-list-chip">Product count</span>
         @break
     
     @case ( $task instanceof task\count )
-        <span class="mdc-list-chip" style="background-color: #3f51b5; color: white;">Count</span>
+        <span class="mdc-list-chip">Count</span>
         @break
 
     @case ( $task instanceof task\protein )
-        <span class="mdc-list-chip" style="background-color: #03a9f4;">Protein</span>
+        <span class="mdc-list-chip">Protein</span>
         @break
 
     @case ( $task instanceof task\daily )
-        <span class="mdc-list-chip" style="background-color: #009688;">Daily</span>
+        <span class="mdc-list-chip">Daily</span>
         @break
 
     @case ( $task instanceof task\due )
-        <span class="mdc-list-chip" style="background-color: #ff9800;">Due</span>
+        <span class="mdc-list-chip">Due</span>
         @break
 
     @default
