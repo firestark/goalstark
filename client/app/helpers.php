77c77f0e42de2e63be72b592e092cec2f6742418
Aron Wouters <lionar@mail.com>
2019-05-19 19:03:54 +0200

    Refactored task list items

---
+++ client/app/helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+function taskToTypeString ( task $task ) : string
+{
+	switch ( $task ) {
+		case $task instanceof task\product\maxKcal:
+			return 'product.max-kcal';
+		case $task instanceof task\product\count:
+			return 'product.count';
+		case $task instanceof task\protein:
+			return 'protein';
+		case $task instanceof task\count:
+			return 'count';
+		case $task instanceof task\daily:
+			return 'daily';
+		case $task instanceof task\due:
+			return 'due';
+		
+		default:
+			throw new \exception ( 'Unknown type' );
+	}
+}
--- client/views/goals/tasks/task.blade.php
+++ client/views/goals/tasks/task.blade.php
@@ -1,59 +1,9 @@
 <li class="mdc-list-item" tabindex="0">
-                        
-    @if ( $task instanceof task\due or ( $task instanceof task\daily and ! $task instanceof task\protein ) )
-        @if ( $task->isCompleted ( ) )
-            <a  href="/tasks/{{ $task->id }}/uncomplete" 
-                class="mdc-list-item__graphic completed" 
-                aria-hidden="true">
-                
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path fill="none" d="M0 0h24v24H0V0z"/>
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.29 16.29L5.7 12.7c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L10 14.17l6.88-6.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-7.59 7.59c-.38.39-1.02.39-1.41 0z"/>
-                </svg>
-            </a>
-        @else
-            <a  href="/tasks/{{ $task->id }}/complete" 
-                class="mdc-list-item__graphic {{ ( isset ( $task->due ) and $task->due < time ( ) ) ? 'overdue' : '' }}" 
-                aria-hidden="true">
-                
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path fill="none" d="M0 0h24v24H0V0z"/>
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.88-11.71L10 14.17l-1.88-1.88c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7c.39-.39.39-1.02 0-1.41-.39-.39-1.03-.39-1.42 0z"/>
-                </svg>
-            </a>
-        @endif
-    @elseif ( $task instanceof task\protein )
-        <span class="mdc-list-item__graphic {{ ( $protein > $task->goal ) ? 'completed' : '' }}" aria-hidden="true">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <path fill="none" d="M0 0h24v24H0V0z"/>
-                <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
-            </svg>
-        </span>
-    @else
-        <a href="/tasks/{{ $task->id }}/complete" class="mdc-list-item__graphic {{ ( $task->isCompleted ( ) ) ? 'completed' : '' }}" aria-hidden="true">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"/>
-                <path d="M10 7H8v4H4v2h4v4h2v-4h4v-2h-4V7zm10 11h-2V7.38L15 8.4V6.7L19.7 5h.3v13z"/>
-            </svg>
-        </a>
-    @endif
+    @include ( 'tasks.types.' . taskToTypeString ( $task ) . '.list-item-action' )
 
     <a href="/tasks/{{ $task->id }}" style="position: relative">
-        <span class="mdc-list-item__text" style="width: 100%;">
-            <span class="mdc-list-item__primary-text">{{ $task->description }}</span>
-            @if ( isset ( $task->times ) )
-                <span class="mdc-list-item__secondary-text">{{ count ( $task->completions ) }} / {{ $task->times }}</span>     
-            @elseif ( isset ( $task->due ) )
-                <span class="mdc-list-item__secondary-text">Due {{  date ( 'M d, Y ', $task->due ) }}</span>
-            @elseif ( isset ( $task->goal ) )
-                <span class="mdc-list-item__secondary-text">Consumed {{ $protein }} of {{ $task->goal }}</span>
-            @else
-                <span class="mdc-list-item__secondary-text">Daily task</span>
-            @endif
-        </span>
-
-            @include ( 'partials.tasks.type-chip', [ 'task' => $task ] )
-        </span>
+        @include ( 'tasks.types.' . taskToTypeString ( $task ) . '.list-item-text' )
+        @include ( 'partials.tasks.type-chip', [ 'task' => $task ] )
     </a>
 
     <a href="/goals/{{ $goal->id }}/tasks/{{ $task->id }}/remove" class="mdc-list-item__meta" aria-hidden="true">
