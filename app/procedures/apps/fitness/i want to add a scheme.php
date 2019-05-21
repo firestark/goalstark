ed3dda3829d25f728261eea49ae230928395ccde
Aron Wouters <lionar@mail.com>
2019-05-21 18:51:04 +0200

    Completing fitness scheme task onm scheme creation

--- app/procedures/apps/fitness/i want to add a scheme.php
+++ app/procedures/apps/fitness/i want to add a scheme.php
@@ -2,8 +2,13 @@
 
 when ( 'i want to add a scheme', then ( apply ( a ( 
     
-function ( scheme $scheme, scheme\manager $manager )
+function ( scheme $scheme, scheme\manager $manager, task\manager $taskManager )
 {
     $manager->add ( $scheme );
+
+    foreach ( $taskManager->all ( ) as $task )
+        if ( $task instanceof task\scheme\create )
+            $taskManager->complete ( $task );
+
     return [ 9002, [ ] ];
 } ) ) ) );
