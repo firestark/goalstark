ffa3a7b15327257d329e69d69358e1eb1f5d9871
Aron Wouters <lionar@mail.com>
2019-05-21 21:27:49 +0200

    Corrected list action

--- client/views/tasks/goals/list.blade.php
+++ client/views/tasks/goals/list.blade.php
@@ -16,27 +16,15 @@
             @foreach ( $goals as $goal )
                 <li class="mdc-list-item" tabindex="0">
 
-                    @if ( $goal->completed )
-                        <a  href="/goals/{{ $goal->id }}/uncomplete" 
-                            class="mdc-list-item__graphic completed" 
-                            aria-hidden="true">
-                            
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <path fill="none" d="M0 0h24v24H0V0z"/>
-                                <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
-                            </svg>
-                        </a>
-                    @else
-                        <a  href="/goals/{{ $goal->id }}/complete" 
-                            class="mdc-list-item__graphic {{ ( $goal->due < time ( ) ) ? 'overdue' : '' }}" 
-                            aria-hidden="true">
-                            
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <path fill="none" d="M0 0h24v24H0V0z"/>
-                                <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
-                            </svg>
-                        </a>
-                    @endif
+                   <span 
+                        class="mdc-list-item__graphic" 
+                        aria-hidden="true">
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="none" d="M0 0h24v24H0V0z"/>
+                            <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
+                        </svg>
+                    </span>
 
                     <a href="/goals/{{ $goal->id }}">
                         <span class="mdc-list-item__text" style="width: 100%;">
