95efa0ad2eaaacf79d3e2cb8b4d8d64cc34dd931
Aron Wouters <lionar@mail.com>
2019-05-22 00:38:43 +0200

    Split dailies into their own section

--- client/statuses/1000 - 2999 goals/1009 - 7009. found a goal's tasks.php
+++ client/statuses/1000 - 2999 goals/1009 - 7009. found a goal's tasks.php
@@ -5,11 +5,12 @@ use function compact as with;
 status::matching ( [ 1009, 7009 ], function ( goal $goal, array $tasks, int $protein )
 {
 	$tasks = array_reverse ( $tasks );
-	$today = array_filter ( $tasks, function ( $task ) { return $task->dueToday ( ); } );
+	$dailies = array_filter ( $tasks, function ( $task ) { return $task instanceof task\daily; } );
+	$today = array_filter ( $tasks, function ( $task ) { return $task->dueToday ( ) and ! $task instanceof task\daily; } );
 	$later = array_filter ( $tasks, function ( $task ) { return $task->dueLater ( ); } );
 	$overdue = array_filter ( $tasks, function ( $task ) { return $task->isOverdue ( ); } );
 	$completed = array_filter ( $tasks, function ( $task ) { return $task->isCompleted ( ); } );
 
-	return view::ok ( 'goals.tasks.list', with ( 'goal', 'tasks', 'today', 'later', 'overdue', 'protein', 'completed' ) );
+	return view::ok ( 'goals.tasks.list', with ( 'goal', 'tasks', 'dailies', 'today', 'later', 'overdue', 'protein', 'completed' ) );
 } );
 
