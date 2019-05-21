e8a82924e8e11d19e498a21b8b5d17512dd60636
Aron Wouters <lionar@mail.com>
2019-05-21 21:31:40 +0200

    Corrected redirect uri

--- client/statuses/3000 - 4999 tasks/3010. successfully merged goals to a task.php
+++ client/statuses/3000 - 4999 tasks/3010. successfully merged goals to a task.php
@@ -5,5 +5,5 @@ use function compact as with;
 status::matching ( 3010, function ( $taskid )
 {
 	session::flash ( 'message', 'Goals merged.' );
-	return redirect::to ( "/tasks/{$taskid}" );
+	return redirect::to ( "/tasks/{$taskid}/goals" );
 } );
