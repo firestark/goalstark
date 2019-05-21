84f85a86fd839ca30ac7376ff5bf4aef4c846345
Aron Wouters <lionar@mail.com>
2019-05-21 18:45:59 +0200

    Updated description

--- app/agreements/task/scheme/create.php
+++ app/agreements/task/scheme/create.php
@@ -6,7 +6,7 @@ class create extends \task\due
 {    
     function __construct ( array $goals, $id, int $due, bool $completed = false )
     {
-        $description = 'Create a fitness scheme.';
+        $description = 'Create a fitness scheme';
         parent::__construct ( $goals, $id, $description, $due, $completed );
     }
 }
