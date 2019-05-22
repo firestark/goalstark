184849bd51e048ab3a46e0c5455bd2ba6fb00e40
Aron Wouters <lionar@mail.com>
2019-05-23 00:35:58 +0200

    Removed deprecated procedures

--- app/procedures/tasks/i want to add goals to a task.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-when ( 'i want to add goals to a task', then ( apply ( a ( 
-    
-function ( array $goals, task $task, task\manager $manager )
-{
-    $manager->merge ( $task, $goals );
-    return [ 3010, [ 'taskid' => $task->id ] ];
-} ) ) ) );
--- app/procedures/tasks/i want to remove a goal from a task.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-when ( 'i want to remove a goal from a task', then ( apply ( a ( 
-    
-function ( goal $goal, task $task, task\manager $taskManager )
-{
-    $taskManager->removeGoalFromTask ( $task, $goal );
-    return [ 3009, [ ] ];
-} ) ) ) );
