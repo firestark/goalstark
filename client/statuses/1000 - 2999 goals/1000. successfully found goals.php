b1b3f2a1e3a57df28ad6b66fabfdada5a059f43a
Aron Wouters <lionar@mail.com>
2019-05-22 14:03:14 +0200

    Corrected status

--- client/statuses/1000 - 2999 goals/1000. successfully found goals.php
+++ client/statuses/1000 - 2999 goals/1000. successfully found goals.php
@@ -4,9 +4,6 @@ use function compact as with;
 
 status::matching ( 1000, function ( array $goals )
 {
-	foreach ( $goals as $goal )
-		$goal->tasks = app::make ( task\manager::class )->tasksFor ( $goal );
-
 	$due = array_filter ( $goals, function ( $goal ) { return ! $goal->isOverdue ( $goal->tasks ) and ! $goal->isCompleted ( $goal->tasks ); } );
 	$overdue = array_filter ( $goals, function ( $goal ) { return $goal->isOverdue ( $goal->tasks ); } );
 	$completed = array_filter ( $goals, function ( $goal ) { return $goal->isCompleted ( $goal->tasks ); } );
