d861eb2381d0d0afa5c850bfade7eb09eb2a8e81
Aron Wouters <lionar@mail.com>
2019-05-23 15:51:41 +0200

    Refactored product count task

--- app/agreements/task/product/count.php
+++ app/agreements/task/product/count.php
@@ -5,20 +5,57 @@ namespace task\product;
 use consumation;
 use product;
 
-class count extends \task\count
+class count extends \task
 {
     public $product = null;
+    public $times = 0;
+    public $due = 0;
 
     function __construct ( $id, product $product, int $times, int $due, array $completions = [ ] )
     {
         $this->product = $product;
+        $this->times = $times;
+        $this->due = endOfDay ( $due );
         $description = "Eat {$product->name}";
 
-        parent::__construct ( $id, $description, $times, $due, $completions );
+        parent::__construct ( $id, $description );
     }
 
     function complete ( )
     {
-        $this->completions [ ] = new consumation ( uniqid ( ), $this->product );
+
+    }
+
+    function uncomplete ( )
+    {
+
+    }
+
+    function isCompleted ( ) : bool
+    {
+        return false;
+    }
+
+    function isOverdue ( ) : bool
+    {
+        return ( 
+            ! $this->isCompleted ( ) and 
+            $this->due < endOfDay ( time ( ) )
+        );
+    }
+
+    function dueToday ( ) : bool
+    {
+        return ( ! $this->isCompleted ( ) and $this->due === endOfDay ( time ( ) ) );
+    }
+
+    function dueLater ( ) : bool
+    {
+        return ( ! $this->isCompleted ( ) and ! $this->isOverdue ( ) and ! $this->dueToday ( ) );
+    }
+
+    function __toString ( )
+    {
+        return uniqid ( );
     }
 }
--- client/views/tasks/types/product/count/list-item-text.blade.php
+++ client/views/tasks/types/product/count/list-item-text.blade.php
@@ -1,4 +1,7 @@
 <span class="mdc-list-item__text" style="width: 100%;">
     <span class="mdc-list-item__primary-text">{{ $task->description }}</span>        
-    <span class="mdc-list-item__secondary-text">{{ count ( $task->completions ) }} / {{ $task->times }}</span>     
+    <span class="mdc-list-item__secondary-text">
+        {{ count ( array_filter ( app::make ( dietitian::class )->today ( ), function ( $consumation ) use ( $task ) { return $consumation->product->id === $task->product->id; } ) ) }} 
+        / {{ $task->times }}
+    </span>     
 </span>
