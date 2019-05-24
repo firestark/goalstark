83b5b6711f8559dad5c01aed3910f8a28d5baafe
Aron Wouters <lionar@mail.com>
2019-05-24 13:57:07 +0200

    Corrected redirect

--- client/statuses/apps/7000 - 8999 food tracker/7006. Registered a consumation.php
+++ client/statuses/apps/7000 - 8999 food tracker/7006. Registered a consumation.php
@@ -5,5 +5,9 @@ use function compact as with;
 status::matching ( 7006, function ( )
 {
 	session::flash ( 'message', 'Consumation added.' );
-	return redirect::to ( '/apps/food-tracker/' );
+
+	if ( session::get ( 'uri', '/' ) === '/apps/food-tracker/add' )
+		return redirect::to ( '/apps/food-tracker/' );
+	
+	return redirect::back ( );
 } );
