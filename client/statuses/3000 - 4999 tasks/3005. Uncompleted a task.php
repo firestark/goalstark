07e8df126e2a3c04416741c91f5d669fa01c0001
Aron Wouters <lionar@mail.com>
2019-05-24 14:20:32 +0200

    Corrected message

--- client/statuses/3000 - 4999 tasks/3005. Uncompleted a task.php
+++ client/statuses/3000 - 4999 tasks/3005. Uncompleted a task.php
@@ -2,13 +2,13 @@
 
 status::matching ( 3005, function ( task $task )
 {
-	$message = 'Task completed.';
+	$message = 'Task uncompleted.';
 
 	if ( $task instanceof task\product\count )
 		$message = 'Removed 1 consumation.';
 	else if ( $task instanceof task\count )
 		$message = '+1.';
 
-	session::flash ( 'message', 'Task uncompleted.' );
+	session::flash ( 'message', $message );
 	return redirect::back ( );
 } );
