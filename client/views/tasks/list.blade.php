e0c047167e713375e5327dface1a9ce04d52e370
Aron Wouters <lionar@mail.com>
2019-05-24 12:19:10 +0200

    Corrected empty list helper text

--- client/views/tasks/list.blade.php
+++ client/views/tasks/list.blade.php
@@ -60,12 +60,8 @@
             </svg>
 
             <h3 class="mdc-typography--subtitle2" style="font-weight: bold; margin: 8px 0 0;">No tasks here</h3>
-            <p class="mdc-typography--body2" style="color: var(--mdc-theme-text-secondary-on-background); margin: 0; line-height: 1.5rem;">Add a tasks by clicking the <span> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: var(--mdc-theme-text-secondary-on-background); position: relative; top: 4px;">
-                    <path fill="none" d="M0 0h24v24H0V0z"/>
-                    <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"/>
-                </svg>
-                </span> button
+            <p class="mdc-typography--body2" style="color: var(--mdc-theme-text-secondary-on-background); margin: 0; line-height: 1.5rem;">
+                Add tasks to a goal in the goals section and they will show up in this overview
             </p>
         </div>
     @endif
