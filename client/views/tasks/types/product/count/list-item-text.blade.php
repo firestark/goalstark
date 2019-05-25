71f767873477de53edab0337961495983fdb5219
Aron Wouters <lionar@mail.com>
2019-05-25 14:00:18 +0200

    Removed space in text

--- client/views/tasks/types/product/count/list-item-text.blade.php
+++ client/views/tasks/types/product/count/list-item-text.blade.php
@@ -4,8 +4,8 @@
             <span style="color: var(--mdc-theme-text-secondary-on-background); font-weight: normal; position: relative; top: 2px; left: 4px; font-size: 0.785rem;">{{ $task->total }}</span>
         @endif
     </span>        
-    <span class="mdc-list-item__secondary-text">
-        {{ count ( array_filter ( app::make ( dietitian::class )->today ( ), function ( $consumation ) use ( $task ) { return $consumation->product->id === $task->product->id; } ) ) }} 
+    <span class="mdc-list-item__secondary-text">{{ 
+        count ( array_filter ( app::make ( dietitian::class )->today ( ), function ( $consumation ) use ( $task ) { return $consumation->product->id === $task->product->id; } ) ) }} 
         / {{ $task->times }}
     </span>     
 </span>
