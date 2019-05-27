a5b811f35f58d15d9ce94edfc99b261b602cb1ce
Aron Wouters <lionar@mail.com>
2019-05-27 23:03:46 +0200

    Corrected variable usage

--- client/views/tasks/types/product/count/nested-list-item--text.blade.php
+++ client/views/tasks/types/product/count/nested-list-item--text.blade.php
@@ -1,4 +1,4 @@
-{{ count ( array_filter ( app::make ( dietitian::class )->today ( ), function ( $consumation ) use ( $task ) { return $consumation->product->id === $task->product->id; } ) ) }} 
+{{ count ( array_filter ( app::make ( dietitian::class )->today ( ), function ( $consumation ) use ( $tasks ) { return $consumation->product->id === $tasks [ 0 ]->product->id; } ) ) }} 
 / {{ max ( array_map ( function ( $task ) { return $task->times; }, $tasks ) ) }} consumed
 
 
