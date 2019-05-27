97a050b90d11094ea4a9ce9600bb0903fb9462ea
Aron Wouters <lionar@mail.com>
2019-05-27 16:01:49 +0200

    Made product count a daily task

--- app/agreements/task/product/count.php
+++ app/agreements/task/product/count.php
@@ -5,17 +5,22 @@ namespace task\product;
 use consumation;
 use product;
 
-class count extends \task\due
+class count extends \task\daily
 {
     public $product = null;
     public $times = 0;
 
-    function __construct ( $id, product $product, int $times, int $due )
+    function __construct ( $id, product $product, int $times )
     {
         $this->product = $product;
         $this->times = $times;
         $description = "Consume {$product->name}";
 
-        parent::__construct ( $id, $description, $due );
+        parent::__construct ( $id, $description );
+    }
+
+    function __toString ( )
+    {
+        return $this->description;
     }
 }
