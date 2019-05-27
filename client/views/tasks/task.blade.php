e180ee0491aa0fb2c28da0529ca16c3546d17dc9
Aron Wouters <lionar@mail.com>
2019-05-27 16:55:59 +0200

    Corrected meta icon

--- client/views/tasks/task.blade.php
+++ client/views/tasks/task.blade.php
@@ -16,12 +16,12 @@
 
                 
 
-                <a href="/goals/{{ $task [ 0 ]->goal->id }}/tasks/{{ $task [ 0 ]->id }}/remove" class="mdc-list-item__meta" aria-hidden="true">
+                <span class="mdc-list-item__meta" aria-hidden="true">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
                         <path d="M0 0h24v24H0z" fill="none"/>
                     </svg>
-                </a>
+                </span>
             </li>
         </label>
 
