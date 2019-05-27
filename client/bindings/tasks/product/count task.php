18398bf3d636286e7083216a953fb90c7e1c1bb6
Aron Wouters <lionar@mail.com>
2019-05-27 17:07:39 +0200

    Updated binding

--- client/bindings/tasks/product/count task.php
+++ client/bindings/tasks/product/count task.php
@@ -5,7 +5,6 @@ app::bind ( task\product\count::class, function ( $app )
     return new task\product\count (
         input::get ( 'taskid', uniqid ( ) ),
         app::make ( productManager::class )->find ( app::make ( product::class ) ),
-        input::get ( 'count', 0 ),
-        strtotime ( input::get ( 'due', date ( 'Y-m-d' ) ) )
+        input::get ( 'count', 0 )
     );
 } );
